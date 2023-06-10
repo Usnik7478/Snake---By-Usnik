ccf460dd64ec5b4d5d4b99a4aef129e6c130774a
Usnik7478 <94280722+Usnik7478@users.noreply.github.com>
2023-06-10 15:43:37 +0530

    Update food.py

--- food.py
+++ food.py
@@ -1,17 +1,21 @@
-from turtle import Turtle 
-from random import randint
+from turtle import Turtle
+from random import randint, choice
+
 
 class Food(Turtle):
+
     def __init__(self):
         super().__init__()
-        self.shape("circle")
+        self.fo = ['circle', 'square', 'triangle']
         self.penup()
-        self.shapesize(0.5,0.5)
-        self.color("blue")
+        self.shapesize(0.5, 0.5)
+        self.color("red")
         self.speed("fastest")
         self.refresh()
 
     def refresh(self):
         self.x = randint(-280, 280)
         self.y = randint(-280, 280)
-        self.goto(self.x,self.y)
+        self.foo = choice(self.fo)
+        self.shape(self.foo)
+        self.goto(self.x, self.y)
