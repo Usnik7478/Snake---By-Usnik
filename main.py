516f8c2b11bc9c7c30ed51230d00682c8ecc8de2
Usnik7478 <94280722+Usnik7478@users.noreply.github.com>
2023-06-10 15:42:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import turtle
-import random
 import time
 from snake import Snake
 from food import Food
 from score import Score
 
-screen = turtle.Screen()  #//--> Setting the screen
-screen.setup(width=600,height=600)
+screen = turtle.Screen()  # //--> Setting the screen
+screen.setup(width=700, height=700)
 screen.bgcolor("black")
 screen.title("Snake 🐍 - By Usnik")
+border = turtle.Turtle()
+border.speed(0)
+border.penup()
+border.hideturtle()
+border.goto(x=-290, y=290)
+border.pencolor('white')
+border.pendown()
+border.setheading(0)
+border.forward(580)
+border.right(90)
+border.forward(580)
+border.right(90)
+border.forward(580)
+border.right(90)
+border.forward(580)
 screen.tracer(0)
 
 snake = Snake()
@@ -28,17 +42,21 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-    if snake.head.distance(food)<15:
+    if snake.head.distance(food) < 15:
         food.refresh()
         score.update()
         snake.extends()
 
-    if snake.head.xcor() > 280 or snake.head.xcor()< -280 or snake.head.ycor() > 280 or snake.head.ycor()< -280:
-        game_is_on = False
-    
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        snake.reset()
+        score.reset()
+        score.showscore()
+
     for i in snake.snake_body[1:]:
         if snake.head.distance(i) < 10:
-            game_is_on =False
+            snake.reset()
+            score.reset()
+            score.showscore()
 
 score.print_fscore()
 
